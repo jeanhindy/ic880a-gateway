936c717e961c1f8969bd43241fce1feec63c5f94
jeanhindy <31793348+jeanhindy@users.noreply.github.com>
2017-10-07 11:19:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Test the connection, wait if needed.
-while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[TTN Gateway]: Waiting for internet connection..."
-  sleep 30
-  done
+#while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
+#  echo "[TTN Gateway]: Waiting for internet connection..."
+#  sleep 30
+#  done
 
 # If there's a remote config, try to update it
 if [ -d ../gateway-remote-config ]; then
